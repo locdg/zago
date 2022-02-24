3f9a6a108d1668491bd89766d53eb7aa4c11b435
Apple <locdg.dev@gmai.com>
2022-02-25 02:32:43 +0700

    init branch dev

--- zago/lib/main.dart
+++ zago/lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'ZAGO',
+      title: 'ZAGO-DEMO',
       debugShowCheckedModeBanner: false,
       localizationsDelegates: context.localizationDelegates,
       supportedLocales: context.supportedLocales,
